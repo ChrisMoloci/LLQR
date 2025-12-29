c14bb5bbc3b70c5300a92323b2ee4f686eed14bb
Chris <chrismoloci@gmail.com>
2025-12-29 18:25:45 -0500

    Made empty starter files for project

---
+++ src/internal/generateFullQR.ts
@@ -0,0 +1,17 @@
+function generateFullQR() {
+    // Determine Mode
+
+    // Encode Data to Binary
+
+    // Determine the Min Version of the QR Code
+
+    // Generate Error Correction Codewords
+
+    // Generate the QR Matrix
+
+    // Generate An Image from the Matrix
+
+    // Return the Image
+}
+
+export default generateFullQR;
