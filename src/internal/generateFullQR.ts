bb5659e092ec48982cf0beb4fdd3338d5ab4498f
Chris <chrismoloci@gmail.com>
2025-12-29 20:18:55 -0500

    Moved logic related to computing the QR Code to generateQRMatrix from generateFullQR and generateFullQR now just uses the generateQRMatrix to compute the qr matrix when called

--- src/internal/generateFullQR.ts
+++ src/internal/generateFullQR.ts
@@ -3,29 +3,11 @@ import autoEncodeData from "../core/autoEncodeData";
 import determineMode from "../core/determineEncodingMode";
 import determineMinQRVersion from "../core/determineMinQRVersion";
 import { qrSpecs } from "../types";
+import generateQRMatrix from "./generateQRMatrix";
 
 function generateFullQR(data: string, specs: qrSpecs = DEFAULT_QR_SPECS) {
-    console.log("Generating QR Code with specs:", specs);
-
-    // Determine Mode
-    const mode: DataEncodingMode = determineMode(data);
-    if (!mode) throw new Error("Unable to determine encoding mode for the provided data.");
-
-    // Encode Data to Binary
-    const encodedData: string[] | undefined = autoEncodeData(data, mode);
-
-    if (!encodedData) throw new Error("Data encoding failed.");
-
-    console.log("Encoded Data:", encodedData);
-
-    // Determine the Min Version of the QR Code
-    const minVersion = determineMinQRVersion(encodedData, specs.eccLevel, mode);
-
-    console.log("Determined Minimum QR Version:", minVersion);
-
-    // Generate Error Correction Codewords
-
     // Generate the QR Matrix
+    const qrMatrix = generateQRMatrix(data, specs);
 
     // Generate An Image from the Matrix
 
---
+++ src/internal/generateQRMatrix.ts
@@ -0,0 +1,28 @@
+import { DataEncodingMode } from "../const";
+import autoEncodeData from "../core/autoEncodeData";
+import determineMode from "../core/determineEncodingMode";
+import determineMinQRVersion from "../core/determineMinQRVersion";
+
+function generateQRMatrix(data: string, specs: qrSpecs = DEFAULT_QR_SPECS){
+        console.log("Generating QR Code with specs:", specs);
+
+    // Determine Mode
+    const mode: DataEncodingMode = determineMode(data);
+    if (!mode) throw new Error("Unable to determine encoding mode for the provided data.");
+
+    // Encode Data to Binary
+    const encodedData: string[] | undefined = autoEncodeData(data, mode);
+
+    if (!encodedData) throw new Error("Data encoding failed.");
+
+    console.log("Encoded Data:", encodedData);
+
+    // Determine the Min Version of the QR Code
+    const minVersion = determineMinQRVersion(encodedData, specs.eccLevel, mode);
+
+    console.log("Determined Minimum QR Version:", minVersion);
+
+    // Generate Error Correction Codewords
+}
+
+export default generateQRMatrix;
