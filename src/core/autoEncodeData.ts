a93cd5728c195f6d5c4720f0a5589e78e8eadf72
Chris <chrismoloci@gmail.com>
2025-12-29 19:04:51 -0500

    Created started function for autoEncodeData.ts

--- src/core/autoEncodeData.ts
+++ src/core/autoEncodeData.ts
@@ -1,7 +1,19 @@
-import { DataEncodingMode } from "../const";
+import { DATA_ENCODING_MODES, DataEncodingMode } from "../const";
 
 function autoEncodeData(data: string, encodingMode: DataEncodingMode) {
-
+    switch (encodingMode) {
+        case DATA_ENCODING_MODES.NUMERIC:
+            break;
+        case DATA_ENCODING_MODES.ALPHANUMERIC:
+            break;
+        case DATA_ENCODING_MODES.BYTE:
+            break;
+        case DATA_ENCODING_MODES.KANJI:
+            // TODO: Implement Kanji encoding
+            break;
+        default:
+            throw new Error("Unsupported encoding mode");
+    }
 }
 
 export default autoEncodeData;
