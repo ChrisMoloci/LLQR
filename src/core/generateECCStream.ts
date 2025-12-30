346b6f868536d4e0f4dc07376e32086dcdf8e8ee
Chris <chrismoloci@gmail.com>
2025-12-30 14:38:13 -0500

    Added data grouping and blocking to prepare for error correction and our data stream is being converted to a stream of integers now

--- src/core/generateECCStream.ts
+++ src/core/generateECCStream.ts
@@ -9,15 +9,21 @@ function generateECCStream(encodedData: Array<string>, qrVersion: qrVersions, ec
     const normalizedDataStream = normalizeDataStream(encodedData, qrVersion, eccLevel!);
     console.log("Normalized Data Stream for ECC Generation:", normalizedDataStream);
 
-    // -- 2. Split data into blocks and groups if applicable --
+    // -- 2. Convert data stream to integers --
+    const dataStreamIntegers: Array<number> = normalizedDataStream.map(byte => parseInt(byte, 2));
+    console.log("Data Stream as Integers for ECC Generation:", dataStreamIntegers);
 
-    // -- 3. Padd ECC 0 bytes for each block --
+    // -- 3. Split data into blocks and groups if applicable --
+    const groupedData: Array<Array<Array<number>>> = groupDataAndBlocks(dataStreamIntegers, qrVersion, eccLevel!);
+    console.log("Grouped Data for ECC Generation:", groupedData);
 
-    // -- 4. Compute ECC for each block in each group --
+    // -- 4. Padd ECC 0 bytes for each block --
 
-    // -- 5. Interleave Stream
+    // -- 5. Compute ECC for each block in each group --
 
-    // -- 6. Return ECC Stream as array of strings --
+    // -- 6. Interleave Stream
+
+    // -- 7. Return ECC Stream as array of strings --
 }
 
 function normalizeDataStream(encodedData: Array<string>, qrVersion: qrVersions, eccLevel: string): Array<string> {
@@ -77,6 +83,45 @@ function normalizeDataStream(encodedData: Array<string>, qrVersion: qrVersions, 
     console.log("Size of Normalized Data Array (in bits):", normalizedDataArray.length * 8);
 
     return normalizedDataArray;
+}
+
+function groupDataAndBlocks(dataStream: Array<number>, qrVersion: qrVersions, eccLevel: string) {
+    const groupingObj = qrDataCapacityBits[qrVersion][eccLevel].blocks;
+
+    // Create the unnormalized groups with data from all blocks in each group
+    const group1 = dataStream.slice(0, (groupingObj.g1.numBlocks * groupingObj.g1.dataCodewordsPerBlock));
+    const group2 = dataStream.slice((groupingObj.g1.numBlocks * groupingObj.g1.dataCodewordsPerBlock), ((groupingObj.g1.numBlocks * groupingObj.g1.dataCodewordsPerBlock)) + ((groupingObj.g2.numBlocks * groupingObj.g2.dataCodewordsPerBlock)));
+
+    // Create group matrix of blocks
+    const groupedData: Array<Array<Array<number>>> = [[], []]; // Array to hold two groups
+
+    // Add seperate blocks to each group
+    for (let group of groupedData) {
+        switch (group) {
+            case groupedData[0]: // Group 1
+                // Create blocks for group 1
+                groupedData[0] = createBlocksForGroup(group1, groupingObj.g1.numBlocks, groupingObj.g1.dataCodewordsPerBlock);
+                break;
+            case groupedData[1]: // Group 2
+                // Create blocks for group 2
+                groupedData[1] = createBlocksForGroup(group2, groupingObj.g2.numBlocks, groupingObj.g2.dataCodewordsPerBlock);  
+                break;
+        }
+    }
+
+    // Helper function to create blocks for a group
+    function createBlocksForGroup(groupData: Array<number>, numBlocks: number, dataCodewordsPerBlock: number): Array<Array<number>> {
+        let blocks: Array<Array<number>> = []; // Will hold all the blocks for the group
+        for (let i = 0; i < numBlocks; i++) {
+            // Get the block from the entire group data
+            const block = groupData.slice(i * dataCodewordsPerBlock, (i + 1) * dataCodewordsPerBlock);
+            blocks.push(block); // Add the block to the blocks array
+        }
+        return blocks; // Return the blocks for the group
+    }
+
+    // Return the grouped data
+    return groupedData
 }
 
 export default generateECCStream;
