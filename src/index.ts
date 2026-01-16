07e3e8e9331d0ce726a5ad983bb1ee5283e78992
Chris <chrismoloci@gmail.com>
2026-01-15 23:23:31 -0500

    Removed no longer existstent encodeData.ts file to prevent build issues

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import generateFullQR from "./internal/generateFullQR";
-import encodeData from "./internal/encodeData";
 import generateQRMatrix from "./internal/generateQRMatrix";
 import generateImageFromQRMatrix from "./internal/generateImageFromQRMatrix";
 import defineConfig from "./core/defineConfig";
@@ -7,7 +6,7 @@ import { QR_DEFAULTS, IMAGE_DEFAULTS, QRDefault, ImageDefault } from "./const";
 import { ECC_LEVEL_CODES, MASK_PATTERN_CODES, ECCLevelCode, ECCLevelKey } from "./enums";
 
 // Export modules
-export { generateFullQR, encodeData, generateQRMatrix, generateImageFromQRMatrix, defineConfig, QR_DEFAULTS, IMAGE_DEFAULTS, ECC_LEVEL_CODES, MASK_PATTERN_CODES };
+export { generateFullQR, generateQRMatrix, generateImageFromQRMatrix, defineConfig, QR_DEFAULTS, IMAGE_DEFAULTS, ECC_LEVEL_CODES, MASK_PATTERN_CODES };
 
 // Export types
 export type { QRDefault, ImageDefault, ECCLevelCode, ECCLevelKey };
