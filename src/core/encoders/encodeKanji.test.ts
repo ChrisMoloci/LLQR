95cb3ed3b527585b0d6bca2928560918858afc09
Chris <chrismoloci@gmail.com>
2026-01-15 23:20:05 -0500

    Create kanji encoder test (does not pass but it's likely due to an issue with encodeKanji, not the test itself

--- src/core/encoders/encodeKanji.test.ts
+++ src/core/encoders/encodeKanji.test.ts
@@ -1,1 +1,15 @@
 import { describe, it, expect } from "vitest";
+import unicodeToShiftJIS from "../../datasets/unicode_to_shiftjis";
+import encodeKanji from "./encodeKanji";
+
+const unicodeKanjiValues = Object.keys(unicodeToShiftJIS).map((key) => String.fromCharCode(parseInt(key)));
+
+describe("Encode Kanji", () => {
+    it("Attempt to encode data from 1 to 6879 kanji characters (all kanji encodable chars)", () => {
+        let testChars: string = "";
+        for (let i = 1 ; i <= unicodeKanjiValues.length; i += 100) {
+            testChars += unicodeKanjiValues.slice(0, i).join('');
+            expect(encodeKanji(testChars)?.charCount).toBe(i);
+        }
+    })
+});
--- src/internal/encodeData.ts
+++
@@ -1,9 +0,0 @@
-function encodeData() {
-    // -- 1. Segment and encode the data --
-
-    // -- 2. Consolidate encoded segments into a singular stream --
-
-    // -- 3. Return the encoded data --
-}
-
-export default encodeData;
