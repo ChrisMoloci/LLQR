8087a6ea2f4714f0b2fb9ea9782f33a80d060f7c
Chris <chrismoloci@gmail.com>
2026-06-29 15:09:52 -0400

    Added documentation to gfMultiply

--- src/ecc/gf256/gfMultiply.ts
+++ src/ecc/gf256/gfMultiply.ts
@@ -2,12 +2,19 @@ import {gf256AntilogTable, gf256LogTable} from "../../datasets";
 
 // Multiply two numbers in GF(256)
 export function gfMultiply(a: number, b: number): number {
+    /**
+     * Formula:
+     * A * B = antilog[(log[A] + log[B]) % 255]
+     */
+
+    // Input validation
     if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || a > 255 || b < 0 || b > 255) {
         throw Error("GF Multiplication error");
     }
 
     if (a === 0 || b === 0) return 0; // If either number is zero, the product is zero
 
+    // Get the log of both numbers
     const aLog = gf256LogTable[a];
     const bLog = gf256LogTable[b];
 
@@ -15,10 +22,11 @@ export function gfMultiply(a: number, b: number): number {
         throw Error("GF Multiplication error");
     }
 
+    // Get the log sum
     const logSum = (aLog + bLog) % 255; // Add the logarithms and wrap around at 255
-    const antilog = gf256AntilogTable[logSum];
+    const antilog = gf256AntilogTable[logSum]; // Get the antilog of the logSum
 
     if (antilog === undefined) throw Error("GF Multiplication error");
 
-    return antilog;
+    return antilog; // Antilog is the final result
 }
